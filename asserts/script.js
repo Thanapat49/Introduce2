7e035ea39f2f3754da8c3361a373d30992e8fb2d
Thanapat49 <Tanapatnaamee@gmail.com>
2024-06-26 19:42:01 +0700

    Update script.js

--- asserts/script.js
+++ asserts/script.js
@@ -1,14 +1,26 @@
-// JavaScript for handling scroll animation
 document.addEventListener('scroll', function() {
-    var elements = document.querySelectorAll('.animate');
-    var windowHeight = window.innerHeight;
-  
-    elements.forEach(function(element) {
-      var positionFromTop = element.getBoundingClientRect().top;
-  
-      if (positionFromTop - windowHeight <= 0) {
-        element.classList.add('show');
-      }
-    });
+  const hiddenContent = document.getElementById('content1');
+  const rect = hiddenContent.getBoundingClientRect();
+  const windowHeight = window.innerHeight || document.documentElement.clientHeight;
+  const Content2 = document.getElementById('content2')
+  const Content3 = document.getElementById('content3')
+  const Content4 = document.getElementById('content4')
+  const rect2 = Content2.getBoundingClientRect();
+  const rect3 = Content3.getBoundingClientRect();
+  const rect4 = Content4.getBoundingClientRect();
+
+  if (rect.top <= windowHeight && rect.bottom >= 0) {
+    hiddenContent.classList.add('visible');
+  } else if (rect2.top >= 0 ) {
+    Content2.classList.add('visible');
+  } else if (rect3.top >= 0) {
+    Content3.classList.add('visible');
+  } else if (rect4.top >= 0) {
+    Content4.classList.add('visible');
+  } else {
+    hiddenContent.classList.add('visible');
+    Content2.classList.add('visible');
+    Content3.classList.add('visible');
+    Content4.classList.add('visible');
+  }
 });
-  
