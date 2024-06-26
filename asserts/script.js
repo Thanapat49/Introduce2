95f3405bc16198cac357d4e973b56786eac2cbae
Thanapat49 <Tanapatnaamee@gmail.com>
2024-06-26 20:07:56 +0700

    Update script.js

--- asserts/script.js
+++ asserts/script.js
@@ -11,12 +11,10 @@ document.addEventListener('scroll', function() {
 
   if (rect.top <= windowHeight && rect.bottom >= 0) {
     hiddenContent.classList.add('visible');
-  } else if (rect2.top >= 0 ) {
     Content2.classList.add('visible');
-  } else if (rect3.top >= 0) {
     Content3.classList.add('visible');
-  } else if (rect4.top >= 0) {
     Content4.classList.add('visible');
+  } 
   } else {
     hiddenContent.classList.add('visible');
     Content2.classList.add('visible');
